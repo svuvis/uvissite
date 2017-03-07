fe1cc620956c27a241084ff67fbfceab8cb6066e
Jelle Breuer <jelle@breuclan.nl>
2017-03-07 12:51:07 +0100

    nieuw bestuur

--- resources/views/pages/bestuur.blade.php
+++ resources/views/pages/bestuur.blade.php
@@ -20,27 +20,43 @@ Bestuur
 			<div class="row centered">
 				<br><br>
 				<div class="col-lg-4">
-                    {!! HTML::image('/img/ik.jpg', 'Charissa', ['class' => 'img-circle']) !!}
-					<h1 class="h7">Charissa van Ingen</h1>
-					 <p>Voorzitter 2014-2015</p>
-					<p><a href="mailto:voorzitter@uvis.nl"><i class="fa fa-envelope" id="mail"></i></a></p>
+                    {!! HTML::image('/img/unknown.jpg', 'Charissa', ['class' => 'img-circle']) !!}
+					<h1 class="h7">William Loosman</h1>
+					 <p>Voorzitter 2016-2017</p>
+					<p><a href="mailto:bestuur@uvis.nl"><i class="fa fa-envelope" id="mail"></i></a></p>
 				</div><!-- col-lg-3 -->
 
 				<div class="col-lg-4">
-                    {!! HTML::image('/img/jelle.jpg', 'Jelle', ['class' => 'img-circle']) !!}
-					<h1 class="h7">Jelle Breuer</h1>
-                    <p>Secretaris 2014-2015</p>
-					<p><a href="mailto:secretaris@uvis.nl"><i class="fa fa-envelope" id="mail"></i></a></p>
+                    {!! HTML::image('/img/nigel.png', 'Jelle', ['class' => 'img-circle']) !!}
+					<h1 class="h7">Nigel van Hattum</h1>
+                    <p>Secretaris 2016-2017</p>
+					<p><a href="mailto:bestuur@uvis.nl"><i class="fa fa-envelope" id="mail"></i></a></p>
 				</div><!-- col-lg-3 -->
 
 				<div class="col-lg-4">
-                    {!! HTML::image('/img/jan.jpg', 'jan', ['class' => 'img-circle']) !!}
-					<h1 class="h7">Jan Goessens</h1>
-                    <p>Penningmeester 2014-2015 </p>
-					<p><a href="mailto:penningmeester@uvis.nl"><i class="fa fa-envelope" id="mail"></i></a></p>
+                    {!! HTML::image('/img/unknown.jpg', 'jan', ['class' => 'img-circle']) !!}
+					<h1 class="h7">Sander de Bruin</h1>
+                    <p>Penningmeester 2016-2017 </p>
+					<p><a href="mailto:bestuur@uvis.nl"><i class="fa fa-envelope" id="mail"></i></a></p>
 				</div><!-- col-lg-3 -->
 
 			</div><!-- row -->
+            <div class="row centered">
+                <br><br>
+                <div class="col-lg-4">
+                    {!! HTML::image('/img/olivier.png', 'Charissa', ['class' => 'img-circle']) !!}
+                    <h1 class="h7">Olivier Verwoerd</h1>
+                     <p>Algemeen lid 2016-2017</p>
+                    <p><a href="mailto:bestuur@uvis.nl"><i class="fa fa-envelope" id="mail"></i></a></p>
+                </div><!-- col-lg-3 -->
+
+                <div class="col-lg-4">
+                    {!! HTML::image('/img/unknown.jpg', 'Jelle', ['class' => 'img-circle']) !!}
+                    <h1 class="h7">Menno van Moort</h1>
+                    <p>Algemeen lid 2016-2017</p>
+                    <p><a href="mailto:bestuur@uvis.nl"><i class="fa fa-envelope" id="mail"></i></a></p>
+                </div><!-- col-lg-3 -->
+            </div><!-- row -->
 			<br>
 			<br>
 		</div><!-- container -->
@@ -51,7 +67,7 @@ Bestuur
 				<div class="col-lg-8 col-lg-offset-2">
 					<h1 class="h7" style="color: #fff;">BESTUUR WORDEN?</h1>
 					<p>
-					    Elk bestuur komt en gaat, dus wil jij graag het bestuur van 2015-2016 worden?
+					    Elk bestuur komt en gaat, dus wil jij graag het bestuur van 2017-2018 worden?
 					    Stel jezelf dan nu verkiesbaar. Dit doe je door een mailtje te sturen naar het huidig
 					    bestuur (bestuur@uvis.nl) met daarin in het kort welke positie je ambieert en motivatie.
 					    Vervolgens sturen wij jou een mailtje terug met meer info.
