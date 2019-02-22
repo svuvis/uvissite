0185c424919f735e43e02ac295ab75acd6ec213d
Jelle Breuer <jelle@breuclan.nl>
2019-02-22 01:12:54 +0100

    Fixed twitter widget

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -852,7 +852,9 @@
                         </svg>
 
 
-                            <div id="tw-widget"></div>
+                            <div id="tw-widget">
+                                <a class="twitter-timeline" href="https://twitter.com/svuvis?ref_src=twsrc%5Etfw">Tweets by svuvis</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                            </div>
                         </div>
                     </div>
                 </div>
